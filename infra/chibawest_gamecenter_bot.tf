5b7e889a7542d1ac83c9a0eac7d4b28645ebf67a
mytk <mytk@pixiv.co.jp>
2023-04-14 18:18:29 +0900

    terraform修正

--- infra/chibawest_gamecenter_bot.tf
+++ infra/chibawest_gamecenter_bot.tf
@@ -25,6 +25,12 @@ resource "google_project_iam_member" "secret_accessor_iam" {
   member  = "serviceAccount:${google_service_account.chibawest_gamecenter_bot_account.email}"
 }
 
+resource "google_project_iam_member" "invoker_iam" {
+  project = local.project
+  role    = "roles/run.invoker"
+  member  = "serviceAccount:${google_service_account.chibawest_gamecenter_bot_account.email}"
+}
+
 resource "google_artifact_registry_repository" "chibawest_gamecenter_bot_repos" {
   provider      = google-beta
   project       = local.project
@@ -33,68 +39,3 @@ resource "google_artifact_registry_repository" "chibawest_gamecenter_bot_repos" 
   description   = "Container Repository of Chibawest Gamecenter Bot"
   format        = "DOCKER"
 }
-
-resource "google_compute_network" "chibawest_gamecenter_bot_network" {
-  name = "chibawest-gamecenter-bot-network"
-}
-
-resource "google_compute_firewall" "chibawest_gamecenter_bot_firewall" {
-  name    = "chibawest-gamecenter-bot-firewall"
-  network = google_compute_network.chibawest_gamecenter_bot_network.name
-
-  allow {
-    protocol = "icmp"
-  }
-
-  # SSH接続用
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_tags = ["chibawest-gamecenter-bot"]
-}
-
-/*
-resource "google_compute_instance" "chibawest_gamecenter_bot_instance" {
-  name         = "chibawest-gamecenter-bot-instance"
-  machine_type = "e2-micro"
-  zone         = local.zone
-  tags         = ["chibawest-gamecenter-bot"]
-
-  # toolboxのgcloudを利用し
-  # https://cloud.google.com/container-optimized-os/docs/concepts/features-and-benefits?hl=ja
-  metadata_startup_script = <<-EOF
-    mkdir -p /var/chibawest-gamecenter-bot
-  
-    toolbox --version \
-    && toolbox -q /google-cloud-sdk/bin/gcloud secrets versions access latest \
-      --secret="chibawest-gamecenter-bot-credentials-json" > \
-      /var/chibawest-gamecenter-bot/google_application_credentials.json \
-    && docker run -d --rm \
-        -v /var/chibawest-gamecenter-bot:/chibawest-gamecenter-bot/data \
-        --name chibawest-gamecenter-bot \
-        mytk0u0/chibawest-gamecenter-bot:latest
-  EOF
-
-  metadata = {
-    enable-oslogin = "TRUE"
-  }
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.chibawest_gamecenter_bot_network.name
-    access_config {}
-  }
-
-  service_account {
-    email  = google_service_account.chibawest_gamecenter_bot_account.email
-    scopes = ["cloud-platform"]
-  }
-}
-*/
